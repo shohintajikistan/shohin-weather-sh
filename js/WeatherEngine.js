9a2c6ae7c274e3ba5923b4989e7a54f932169864
SHOHIN HAMROH <iskandarusupov90@gmail.com>
2026-08-15 15:10:16 +0500

    Update WeatherEngine.js

--- js/WeatherEngine.js
+++ js/WeatherEngine.js
@@ -1,50 +1,297 @@
-import { DataManager } from "./DataManager.js";
-import { ECMWF } from "./ECMWF.js";
+/*
+====================================================
+ SHOHIN WEATHER V5.9
+ WeatherEngine.js
+ REAL WEATHER API ENGINE
+====================================================
+*/
 
 export class WeatherEngine {
 
-    constructor() {
+    constructor(options = {}) {
 
-        this.data = new DataManager();
+        this.latitude =
+            Number(
+                options.latitude ?? 38.5598
+            );
 
-        this.ecmwf = new ECMWF();
+        this.longitude =
+            Number(
+                options.longitude ?? 68.7870
+            );
 
-        this.location = {
+        this.forecastDays =
+            Number(
+                options.forecastDays ?? 7
+            );
 
-            latitude: 38.5598,
-            longitude: 68.7870,
-            name: "Dushanbe"
+        this.baseURL =
+            "https://api.open-meteo.com/v1/forecast";
+
+        this.data = null;
+
+    }
+
+
+    /*
+    ================================================
+    LOAD WEATHER
+    ================================================
+    */
+
+    async load(
+        latitude = this.latitude,
+        longitude = this.longitude
+    ) {
+
+        const params = new URLSearchParams({
+
+            latitude:
+                latitude,
+
+            longitude:
+                longitude,
+
+            forecast_days:
+                this.forecastDays,
+
+            timezone:
+                "auto",
+
+            hourly:
+                [
+                    "temperature_2m",
+                    "relative_humidity_2m",
+                    "precipitation",
+                    "rain",
+                    "showers",
+                    "snowfall",
+                    "cloud_cover",
+                    "wind_speed_10m",
+                    "wind_direction_10m",
+                    "wind_gusts_10m",
+                    "pressure_msl",
+                    "surface_pressure",
+                    "visibility",
+                    "uv_index",
+                    "uv_index_clear_sky",
+                    "cape",
+                    "weather_code"
+                ].join(","),
+
+            daily:
+                [
+                    "weather_code",
+                    "temperature_2m_max",
+                    "temperature_2m_min",
+                    "apparent_temperature_max",
+                    "apparent_temperature_min",
+                    "sunrise",
+                    "sunset",
+                    "uv_index_max",
+                    "precipitation_sum",
+                    "rain_sum",
+                    "showers_sum",
+                    "snowfall_sum",
+                    "wind_speed_10m_max",
+                    "wind_gusts_10m_max",
+                    "wind_direction_10m_dominant"
+                ].join(",")
+
+        });
+
+
+        const url =
+            this.baseURL +
+            "?" +
+            params.toString();
+
+
+        const response =
+            await fetch(url);
+
+
+        if (!response.ok) {
+
+            throw new Error(
+
+                "Weather API HTTP " +
+                response.status
+
+            );
+
+        }
+
+
+        const data =
+            await response.json();
+
+
+        if (
+            !data ||
+            !data.hourly
+        ) {
+
+            throw new Error(
+                "WeatherEngine: invalid API response"
+            );
+
+        }
+
+
+        this.data =
+            data;
+
+
+        return data;
+
+    }
+
+
+    /*
+    ================================================
+    GET DATA
+    ================================================
+    */
+
+    getData() {
+
+        return this.data;
+
+    }
+
+
+    /*
+    ================================================
+    GET CURRENT
+    ================================================
+    */
+
+    getCurrent() {
+
+        if (!this.data?.hourly) {
+
+            return null;
+
+        }
+
+
+        const hourly =
+            this.data.hourly;
+
+
+        return {
+
+            temperature:
+                hourly.temperature_2m?.[0],
+
+            humidity:
+                hourly.relative_humidity_2m?.[0],
+
+            precipitation:
+                hourly.precipitation?.[0],
+
+            rain:
+                hourly.rain?.[0],
+
+            clouds:
+                hourly.cloud_cover?.[0],
+
+            windSpeed:
+                hourly.wind_speed_10m?.[0],
+
+            windDirection:
+                hourly.wind_direction_10m?.[0],
+
+            windGust:
+                hourly.wind_gusts_10m?.[0],
+
+            pressure:
+                hourly.pressure_msl?.[0],
+
+            visibility:
+                hourly.visibility?.[0],
+
+            uv:
+                hourly.uv_index?.[0],
+
+            uvClearSky:
+                hourly.uv_index_clear_sky?.[0],
+
+            cape:
+                hourly.cape?.[0],
+
+            weatherCode:
+                hourly.weather_code?.[0]
 
         };
 
-        this.weather = null;
+    }
+
+
+    /*
+    ================================================
+    GET DAILY
+    ================================================
+    */
+
+    getDaily() {
+
+        return this.data?.daily || null;
 
     }
 
 
-    async load() {
+    /*
+    ================================================
+    GET LOCATION
+    ================================================
+    */
 
-        const key =
-            `${this.location.latitude},${this.location.longitude}`;
+    getLocation() {
 
-        this.weather =
-            await this.data.get(
-                key,
-                () =>
-                    this.ecmwf.getWeather(
-                        this.location.latitude,
-                        this.location.longitude
-                    )
-            );
+        return {
 
-        return this.weather;
+            latitude:
+                this.latitude,
+
+            longitude:
+                this.longitude
+
+        };
 
     }
 
 
-    getWeather() {
+    /*
+    ================================================
+    SET LOCATION
+    ================================================
+    */
 
-        return this.weather;
+    setLocation(
+        latitude,
+        longitude
+    ) {
+
+        this.latitude =
+            Number(latitude);
+
+        this.longitude =
+            Number(longitude);
+
+    }
+
+
+    /*
+    ================================================
+    RELOAD
+    ================================================
+    */
+
+    async reload() {
+
+        return await this.load();
 
     }
 
