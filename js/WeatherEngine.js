9f4aaad9c7a3a5a48516245aec35e6f29b626db0
SHOHIN HAMROH <iskandarusupov90@gmail.com>
2026-08-15 09:56:50 +0500

    Update WeatherEngine.js

--- js/WeatherEngine.js
+++ js/WeatherEngine.js
@@ -1,63 +1,27 @@
-import { DataManager } from "./DataManager.js";
-import { ECMWF } from "./ECMWF.js";
-
 export class WeatherEngine {
 
     constructor() {
 
-        this.data = new DataManager();
-
-        this.ecmwf = new ECMWF();
-
         this.location = {
             latitude: 38.5598,
-            longitude: 68.7870
+            longitude: 68.7870,
+            name: "Dushanbe"
         };
 
         this.weather = null;
 
     }
 
-
     async load() {
 
-        const key =
-            `${this.location.latitude},${this.location.longitude}`;
+        console.log(
+            "SHOHIN WeatherEngine работает!"
+        );
 
-        this.weather =
-            await this.data.get(
-                key,
-                () =>
-                    this.ecmwf.getWeather(
-                        this.location.latitude,
-                        this.location.longitude
-                    )
-            );
-
-        return this.weather;
-
-    }
-
-
-    async setLocation(
-        latitude,
-        longitude
-    ) {
-
-        this.location.latitude =
-            latitude;
-
-        this.location.longitude =
-            longitude;
-
-        return this.load();
-
-    }
-
-
-    getWeather() {
-
-        return this.weather;
+        return {
+            latitude: this.location.latitude,
+            longitude: this.location.longitude
+        };
 
     }
 
